8a84ca12a01dc574a877ad26fc535da1cebbc2a9
Bot <realafe21@gmail.com>
2024-04-05 23:25:06 +0100

    updated explore section

--- pages/explore.vue
+++ pages/explore.vue
@@ -116,6 +116,110 @@ votepanel/"
         </div>
       </div>
       <div class="ss-content-2">
+        <div class="flex w-full items-center justify-between">
+          <div class="flex items-center">
+            <q-avatar color="transparent" size="84">
+              <q-img src="/not-5.png" alt="avatar Image"/>
+            </q-avatar>
+            <div class="ml-3">
+              <div class="font-medium"><span class="text-acc">@yungBo$$Fly</span> just built a new sitadel</div>
+              <div class="text-error">@FLYBOYNATION</div>
+            </div>
+          </div>
+          <q-btn class="text-sec" dense flat round icon="o_more_vert"/>
+        </div>
+        <div class="user-cards mt-5">
+          <h3 class="text-2xl mb-5 font-semibold text-gray-500">Sitizens You May Like To Watch</h3>
+          <div class="grid grid-cols-2 w-full gap-5">
+            <div class="w-full max-w-sm bg-theme mt-3 rounded-lg shadow">
+              <div class="flex justify-end px-4 pt-4">
+                <button id="dropdownButton" data-dropdown-toggle="dropdown"
+                        class=""
+                        type="button">
+                  <span class="sr-only">Open dropdown</span>
+                  <q-btn class="text-sec" dense flat round icon="o_more_horiz"/>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdown"
+                     class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul class="py-2" aria-labelledby="dropdownButton">
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                    </li>
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export
+                        Data</a>
+                    </li>
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="flex flex-col items-center pb-10 px-5">
+                <img class="rounded-full shadow-lg w-full" src="/watch-1.png"
+                     alt="Bonnie image"/>
+                <h5 class="mb-1 mt-5 text-xl font-medium text-gray-900">Anna Fadugba</h5>
+                <span class="text-sm text-acc">@peng_anny</span>
+                <span class="text-sm text-sec">Bingham University - Student</span>
+                <div class="flex mt-4 md:mt-6">
+                  <a href="#"
+                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Watch</a>
+                  <a href="#"
+                     class="py-2 px-4 ms-2 text-sm font-medium text-gray-900">Add</a>
+                </div>
+              </div>
+            </div>
+            <div class="w-full max-w-sm bg-theme mt-3 rounded-lg shadow">
+              <div class="flex justify-end px-4 pt-4">
+                <button id="dropdownButton" data-dropdown-toggle="dropdown"
+                        class=""
+                        type="button">
+                  <span class="sr-only">Open dropdown</span>
+                  <q-btn class="text-sec" dense flat round icon="o_more_horiz"/>
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdown"
+                     class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul class="py-2" aria-labelledby="dropdownButton">
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                    </li>
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export
+                        Data</a>
+                    </li>
+                    <li>
+                      <a href="#"
+                         class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="flex flex-col items-center pb-10 px-5">
+                <img class="rounded-full shadow-lg w-full" src="/watch-2.png"
+                     alt="Bonnie image"/>
+                <h5 class="mb-1 mt-5 text-xl font-medium text-gray-900">Esther Percy</h5>
+                <span class="text-sm text-acc">@theperksofesther</span>
+                <span class="text-sm text-sec">Covenant University - Alumni</span>
+                <div class="flex mt-4 md:mt-6">
+                  <a href="#"
+                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Watch</a>
+                  <a href="#"
+                     class="py-2 px-4 ms-2 text-sm font-medium text-gray-900">Add</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="mt-5 flex justify-center">
+            <q-btn flat dense class="text-sec rounded-2xl px-5">SEE ALL</q-btn>
+          </div>
+        </div>
       </div>
     </div>
   </div>
